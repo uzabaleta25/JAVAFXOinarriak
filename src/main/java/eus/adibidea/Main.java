b5af0a749f30527b7f67b17e1abf2b452c44fd5c
Unai Zabaleta <uzabaleta25@izarraitz.eus>
2026-09-23 09:42:48 +0200

    pane layout(absolutua)

--- src/main/java/eus/adibidea/Main.java
+++ src/main/java/eus/adibidea/Main.java
@@ -1,5 +1,8 @@
 package eus.adibidea;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.application.Application;
@@ -8,7 +11,17 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage){
-        StackPane root = new StackPane();
+        Pane root = new Pane();
+        Label label = new Label("Kaixo!");
+        label.setLayoutX(100);
+        label.setLayoutY(100);
+
+        Button button = new Button("Ados!");
+        button.setLayoutX(300);
+        button.setLayoutY(300);
+
+        root.getChildren().addAll(label, button);
+
         Scene scene = new Scene(root);
 
         stage.setTitle("JavaFX aplikazioa");
