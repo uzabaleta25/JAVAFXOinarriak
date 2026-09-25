28a73411f63e6ceaa70b272d4d5dc74e574321fe
Unai Zabaleta <uzabaleta25@izarraitz.eus>
2026-09-25 08:59:35 +0200

    BorderPane layout-a (TOP, CENTER, BOTTOM) + TextField eta TextArea

--- src/main/java/eus/adibidea/Main.java
+++ src/main/java/eus/adibidea/Main.java
@@ -2,25 +2,42 @@ package eus.adibidea;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.application.Application;
+
+import javax.swing.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage){
-        Pane root = new Pane();
-        Label label = new Label("Kaixo!");
-        label.setLayoutX(100);
-        label.setLayoutY(100);
+        BorderPane root = new BorderPane();
 
-        Button button = new Button("Ados!");
-        button.setLayoutX(300);
-        button.setLayoutY(300);
+        Label label = new Label("Kaixo");
+        root.setTop(label);
 
-        root.getChildren().addAll(label, button);
+
+
+        GridPane grid = new GridPane();
+        Label izenaLbl = new Label("Izena: ");
+        TextField izenaTF = new TextField("");
+        Label abizenaLbl = new Label("Abizena: ");
+        TextField abizenaTF = new TextField("");
+        grid.add(izenaLbl,  0, 0);
+        grid.add(izenaTF,  1, 0);
+        grid.add(abizenaLbl,  0, 1);
+        grid.add(abizenaTF,  1, 1);
+
+        root.setCenter(grid);
+
+        HBox hBox = new HBox();
+        Button adosBttn = new Button("Ados!");
+        Button utziBttn = new Button("Utzi!");
+        hBox.getChildren().addAll(adosBttn, utziBttn);
+        root.setBottom(hBox);
 
         Scene scene = new Scene(root);
 
